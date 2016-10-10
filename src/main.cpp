69b36f36a3fd90151fa2e91a85fd1fca3db2d5c7
Johnson Lau <jl2012@xbt.hk>
2016-10-10 19:29:50 +0800

    Check IsWitnessStandard even before activation
    No witness tx should be relayed before activation but if it does, there is no harm to apply the standard limit (to-be-squashed)

--- src/main.cpp
+++ src/main.cpp
@@ -1261,7 +1261,7 @@ bool AcceptToMemoryPoolWorker(CTxMemPool& pool, CValidationState& state, const C
             return state.Invalid(false, REJECT_NONSTANDARD, "bad-txns-nonstandard-inputs");
 
         // Check for non-standard witness in P2WSH
-        if (witnessEnabled && !tx.wit.IsNull() && fRequireStandard && !IsWitnessStandard(tx, view))
+        if (!tx.wit.IsNull() && fRequireStandard && !IsWitnessStandard(tx, view))
             return state.DoS(0, false, REJECT_NONSTANDARD, "bad-witness-nonstandard", true);
 
         int64_t nSigOpsCost = GetTransactionSigOpCost(tx, view, STANDARD_SCRIPT_VERIFY_FLAGS);
