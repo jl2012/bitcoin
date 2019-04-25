101c496c57269b0d0af2852ca6d913064e96f3ac
Johnson Lau <jl2012@xbt.hk>
2019-04-26 02:13:56 +0800

    activate NOINPUT for testing

--- src/validation.cpp
+++ src/validation.cpp
@@ -1766,6 +1766,7 @@ static unsigned int GetBlockScriptFlags(const CBlockIndex* pindex, const Consens
     // Start enforcing Taproot using versionbits logic.
     if (VersionBitsState(pindex->pprev, consensusparams, Consensus::DEPLOYMENT_TAPROOT, versionbitscache) == ThresholdState::ACTIVE) {
         flags |= SCRIPT_VERIFY_TAPROOT;
+        flags |= SCRIPT_VERIFY_NOINPUT;
     }
 
     if (IsNullDummyEnabled(pindex->pprev, consensusparams)) {
